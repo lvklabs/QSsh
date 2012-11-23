e332f7effb0dc819c95b30900be61d97253652e8
Andres Pagliano <andres.pagliano@lvklabs.com>
2012-11-23 12:26:16 -0300

    Added copyright to SecureUploader Example

--- examples/SecureUploader/securefileuploader.h
+++ examples/SecureUploader/securefileuploader.h
@@ -1,3 +1,21 @@
+/**************************************************************************
+**
+** This file is part of QSsh
+**
+** Copyright (c) 2012 LVK
+**
+** Contact: andres.pagliano@lvklabs.com
+**
+** GNU Lesser General Public License Usage
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this file.
+** Please review the following information to ensure the GNU Lesser General
+** Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
 #ifndef SECUREFILEUPLOADER_H
 #define SECUREFILEUPLOADER_H
 
