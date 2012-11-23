9b3fd9c54bd253240c002509ffd30b0db32bdffc
Andres Pagliano <andres.pagliano@lvklabs.com>
2012-11-23 12:10:57 -0300

    Fixed params order in SecureUploader example plus some refactoring

--- examples/SecureUploader/main.cpp
+++ examples/SecureUploader/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <QStringList>
 
-void showUsage();
+void showSyntax();
 QString getPassword();
 void upload(const QString &orig, const QString &dest, const QString passwd);
 
@@ -14,8 +14,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     if (argc != 3) {
-        showUsage();
-        return 1;
+        showSyntax();
     }
 
     QString orig = argv[1];
@@ -28,15 +27,17 @@ int main(int argc, char *argv[])
 }
 
 
-void showUsage()
+void showSyntax()
 {
-    std::cerr << "Usage:   SecureUploader localfile username@host:/destination" << std::endl;
+    std::cerr << "Syntax: " << std::endl;
+    std::cerr << "         SecureUploader local_file username@host:/destination_path" << std::endl;
+    exit(1);
 }
 
 
 QString getPassword()
 {
-    std::cout << "Password: (Warning password will be echoed)" << std::endl;
+    std::cout << "Password (Warning password will be echoed): ";
     std::string passwd;
     std::cin >> passwd;
 
@@ -55,13 +56,13 @@ void upload(const QString &orig, const QString &dest, const QString passwd)
 
         if (l2.size() == 2) {
             static SecureFileUploader uploader;
-            uploader.upload(orig, l1[0], l2[0], l2[1], passwd);
+            uploader.upload(orig, l2[1], l2[0], l1[0], passwd);
         } else {
-            std::cerr << " SecureUploader:  Error invalid destination " << dest.toStdString() << std::endl;
-            showUsage();
+            std::cerr << "SecureUploader:  Error invalid parameter " << dest.toStdString() << std::endl;
+            showSyntax();
         }
     } else {
-        std::cerr << " SecureUploader:  Error invalid destination " << dest.toStdString() << std::endl;
-        showUsage();
+        std::cerr << "SecureUploader:  Error invalid parameter " << dest.toStdString() << std::endl;
+        showSyntax();
     }
 }
--- examples/SecureUploader/securefileuploader.cpp
+++ examples/SecureUploader/securefileuploader.cpp
@@ -10,8 +10,6 @@ SecureFileUploader::SecureFileUploader(QObject *parent) :
 void SecureFileUploader::upload(const QString &localFile, const QString &dest, const QString &host,
                                 const QString &username, const QString &passwd)
 {
-    qDebug() << localFile << dest << host << username << passwd;
-
     m_localFilename = localFile;
     m_remoteFilename = dest + "/" + localFile;
 
@@ -28,15 +26,15 @@ void SecureFileUploader::upload(const QString &localFile, const QString &dest, c
     connect(m_connection, SIGNAL(connected()), SLOT(onConnected()));
     connect(m_connection, SIGNAL(error(QSsh::SshError)), SLOT(onConnectionError(QSsh::SshError)));
 
-    qDebug() << " SecureUploader: Connecting to host" << host;
+    qDebug() << "SecureUploader: Connecting to host" << host;
 
     m_connection->connectToHost();
 }
 
 void SecureFileUploader::onConnected()
 {
-    qDebug() << " SecureUploader: Connected";
-    qDebug() << " SecureUploader: Creating SFTP channel...";
+    qDebug() << "SecureUploader: Connected";
+    qDebug() << "SecureUploader: Creating SFTP channel...";
 
     m_channel = m_connection->createSftpChannel();
 
@@ -51,38 +49,38 @@ void SecureFileUploader::onConnected()
         m_channel->initialize();
 
     } else {
-        qDebug() << " SecureUploader: Error null channel";
+        qDebug() << "SecureUploader: Error null channel";
     }
 }
 
 void SecureFileUploader::onConnectionError(QSsh::SshError err)
 {
-    qDebug() << " SecureUploader: Connection error" << err;
+    qDebug() << "SecureUploader: Connection error" << err;
 }
 
 void SecureFileUploader::onChannelInitialized()
 {
-    qDebug() << " SecureUploader: Channel Initialized";
+    qDebug() << "SecureUploader: Channel Initialized";
 
     // TODO Choose the overwrite mode: SftpOverwriteExisting, SftpAppendToExisting, SftpSkipExisting
     QSsh::SftpJobId job = m_channel->uploadFile(m_localFilename, m_remoteFilename,
                                                 QSsh::SftpOverwriteExisting);
 
     if (job != QSsh::SftpInvalidJob) {
-        qDebug() << " SecureUploader: Starting job #" << job;
+        qDebug() << "SecureUploader: Starting job #" << job;
     } else {
-        qDebug() << " SecureUploader: Invalid Job";
+        qDebug() << "SecureUploader: Invalid Job";
     }
 }
 
 void SecureFileUploader::onChannelError(const QString &err)
 {
-    qDebug() << " SecureUploader: Error: " << err;
+    qDebug() << "SecureUploader: Error: " << err;
 }
 
 void SecureFileUploader::onOpfinished(QSsh::SftpJobId job, const QString &err)
 {
-    qDebug() << " SecureUploader: Finished job #" << job << ":" << (err.isEmpty() ? "OK" : err);
+    qDebug() << "SecureUploader: Finished job #" << job << ":" << (err.isEmpty() ? "OK" : err);
 }
 
 
