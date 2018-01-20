qt-creator/qt-creator

SSH: Implement tunneling.
This is the "direct-tcpip" port forwarding
specified in RFC 4254.

Change-Id: I1ffa2e923b4479c7211b1b4304e66895b565fb64
Reviewed-by: hjk <qthjk@ovi.com>
Reviewed-by: Christian Kandeler <christian.kandeler@nokia.com>

src/libs/ssh/ssh.pro
@@ -29,7 +29,8 @@ SOURCES = $$PWD/sshsendfacility.cpp \
     $$PWD/sshconnectionmanager.cpp \
     $$PWD/sshkeypasswordretriever.cpp \
     $$PWD/sftpfilesystemmodel.cpp \
-    $$PWD/sshkeycreationdialog.cpp
+    $$PWD/sshkeycreationdialog.cpp \
+    $$PWD/sshdirecttcpiptunnel.cpp
 
 HEADERS = $$PWD/sshsendfacility_p.h \
     $$PWD/sshremoteprocess.h \
@@ -62,6 +63,8 @@ HEADERS = $$PWD/sshsendfacility_p.h \
     $$PWD/sshkeypasswordretriever_p.h \
     $$PWD/sftpfilesystemmodel.h \
     $$PWD/sshkeycreationdialog.h \
-    $$PWD/ssh_global.h
+    $$PWD/ssh_global.h \
+    $$PWD/sshdirecttcpiptunnel_p.h \
+    $$PWD/sshdirecttcpiptunnel.h
 
 FORMS = $$PWD/sshkeycreationdialog.ui

lvklabs/QSsh

src/libs/ssh/ssh.pro
@@ -34,7 +34,9 @@ SOURCES = $$PWD/sshsendfacility.cpp \
     $$PWD/sshremoteprocessrunner.cpp \
     $$PWD/sshconnectionmanager.cpp \
     $$PWD/sshkeypasswordretriever.cpp \
-    $$PWD/sftpfilesystemmodel.cpp
+    $$PWD/sftpfilesystemmodel.cpp \
+    $$PWD/sshdirecttcpiptunnel.cpp
+
 
 HEADERS = $$PWD/sshsendfacility_p.h \
     $$PWD/sshremoteprocess.h \
@@ -66,6 +68,8 @@ HEADERS = $$PWD/sshsendfacility_p.h \
     $$PWD/sshpseudoterminal.h \
     $$PWD/sshkeypasswordretriever_p.h \
     $$PWD/sftpfilesystemmodel.h \
+    $$PWD/sshdirecttcpiptunnel_p.h \
+    $$PWD/sshdirecttcpiptunnel.h \
     $$PWD/ssh_global.h
 
 